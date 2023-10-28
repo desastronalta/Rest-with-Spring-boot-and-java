6053ef4d3e43666dc803c85db0e1747b100fc53d
Rafael Dantas <rafadantasdasilva54@gmail.com>
2023-10-28 11:21:49 -0300

    criação dos metodos crud

--- 05_firstStepsWithSpringBootAndJava/Rest-with-Spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
+++ 05_firstStepsWithSpringBootAndJava/Rest-with-Spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
@@ -1,5 +1,7 @@
 package br.com.erudio.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -13,7 +15,28 @@ public class PersonServices {
 	private final AtomicLong counter = new AtomicLong();
 	private Logger logger = Logger.getLogger(PersonServices.class.getName());
 	
+	public List<Person> findAll() {
+		
+		logger.info("Finding all Persons");
+		
+		List<Person> persons = new ArrayList<>();
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		return persons ;
+	}
+	private Person mockPerson(int i) {
+		Person person = new Person();
+		person .setId(counter.incrementAndGet());
+		person.setFirstName("parsonName" + i);
+		person.setLastName("lastName" + i);
+		person.setAdress("adress" + i);
+		person.setGender("gender" + i);
+		return person;
+	}
 	public Person findById(String id) {
+		
 		
 		logger.info("Finding one Person");
 		
@@ -24,5 +47,23 @@ public class PersonServices {
 		person.setAdress("Floripa, Sao paulo");
 		person.setGender("Male");
 		return person;
+	}
+	public Person create(Person person) {
+		//quando houver acesso a base de dados aqui vira a implementação do mesmo.
+		logger.info("Creating one person");
+		
+		return person;
+	}
+	public Person update(Person person) {
+		//quando houver acesso a base de dados aqui vira a implementação do mesmo.
+		logger.info("Updating one person");
+		
+		return person;
+	}
+	
+	public void delete(String id) {
+		//quando houver acesso a base de dados aqui vira a implementação do mesmo.
+		logger.info("Deleting one person");
+		
 	}
 }
