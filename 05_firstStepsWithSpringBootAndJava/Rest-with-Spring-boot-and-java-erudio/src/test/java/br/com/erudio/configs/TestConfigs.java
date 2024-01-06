28c5f666aa132716c84e00269a472afaadaaa606
Rafael Dantas <rafadantasdasilva54@gmail.com>
2024-01-06 09:15:34 -0300

    test de integração para criação da documentação do Swagger utilizando o docker

---
+++ 05_firstStepsWithSpringBootAndJava/Rest-with-Spring-boot-and-java-erudio/src/test/java/br/com/erudio/configs/TestConfigs.java
@@ -0,0 +1,8 @@
+package br.com.erudio.configs;
+
+public class TestConfigs {
+ /*
+  * classe criada para passar as configurações da porta utilizada nos testes.
+  */
+	public static final int SERVER_PORT = 8888;
+}
