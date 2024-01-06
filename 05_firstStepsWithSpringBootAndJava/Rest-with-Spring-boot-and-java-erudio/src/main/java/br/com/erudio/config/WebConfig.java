573cff76b979588a074f0f21767d7161541b0c80
Rafael Dantas <rafadantasdasilva54@gmail.com>
2024-01-06 14:19:30 -0300

    integração de test cors

--- 05_firstStepsWithSpringBootAndJava/Rest-with-Spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/WebConfig.java
+++ 05_firstStepsWithSpringBootAndJava/Rest-with-Spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/WebConfig.java
@@ -1,8 +1,10 @@
 package br.com.erudio.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /*
@@ -12,8 +14,31 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
 	
+	private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
+
+	/*
+	 * -
+	 */
+	@Value("${cors.originPatterns: default}")
+	private String corsOriginPatterns = "";
+	
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		var allowedOrigins = corsOriginPatterns.split(",");
+		registry.addMapping("/**")
+		.allowedMethods("*")
+		.allowedOrigins(allowedOrigins)
+		.allowCredentials(true);
+		//.allowedMethods("GET" , "POST" , "PUT") para adicionar métodos especificos 
+	}
+	
+//	@Override
+//	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+//		converters.add(new YamlJackson2HttpMesageConverter());
+//	}
+
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 		
@@ -22,7 +47,8 @@ public class WebConfig implements WebMvcConfigurer {
 		.useRegisteredExtensionsOnly(false)
 		.defaultContentType(MediaType.APPLICATION_JSON)
 		.mediaType("json",MediaType.APPLICATION_JSON)
-		.mediaType("xml", MediaType.APPLICATION_XML);
+		.mediaType("xml", MediaType.APPLICATION_XML)
+		.mediaType("x-yml", MEDIA_TYPE_APPLICATION_YML);
 		
 	}
 	
--- 05_firstStepsWithSpringBootAndJava/Rest-with-Spring-boot-and-java-erudio/src/test/java/br/com/erudio/configs/TestConfigs.java
+++ 05_firstStepsWithSpringBootAndJava/Rest-with-Spring-boot-and-java-erudio/src/test/java/br/com/erudio/configs/TestConfigs.java
@@ -2,7 +2,13 @@ package br.com.erudio.configs;
 
 public class TestConfigs {
  /*
-  * classe criada para passar as configurações da porta utilizada nos testes.
+  * classe criada para passar as configurações utilizadas nos testes de integração.
   */
 	public static final int SERVER_PORT = 8888;
+	public static final String HEADER_PARAM_AUTHORIZATION = "Athorization";
+	public static final String HEADER_PARAM_ORIGIN = "Origin";
+	public static final String CONTENT_TYPE_JSON = "application/json";
+	public static final String CONTENT_TYPE_XML = "application/xml";
+	public static final String CONTENT_TYPE_YML = "application/x-yml";
+	
 }
